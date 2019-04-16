c7d281c5682093af3c0e98bd53c4193b06e3c45c
SunARcTech2 <sunarctech2@SunARcTech2s-Mac-mini.local>
2019-04-16 12:44:03 +0530

    Bottom sheets

---
+++ 15. bottom_sheets/lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+//https://www.youtube.com/watch?v=KpR5fQx_V2c
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  createState() => MyHomePageState(); 
+}
+
+class MyHomePageState extends State<MyHomePage> {
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  VoidCallback _showPersistanceCallBack;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _showPersistanceCallBack = _showBottomSheet;
+  }
+
+  void _showBottomSheet() {
+
+    setState(() {
+      _showPersistanceCallBack = null;
+    });
+
+    _scaffoldKey.currentState.showBottomSheet((context) {
+      return Container(
+        height: 350,
+        color: Colors.greenAccent,
+        child: Center(
+          child: Text("Hi botomsheet"),
+        ),
+      );
+    }).closed.whenComplete(() {
+      setState(() {
+        _showPersistanceCallBack = _showBottomSheet;
+      });
+    });
+
+  }
+
+  void _showModelSheet() {
+    showModalBottomSheet(context: context, builder: (builder) {
+      return Container(
+        height: 800,
+        color: Colors.greenAccent,
+        child: Center(
+          child: Text("Hi botomsheet"),
+        ),
+      );
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(title: Text("App bar"),),
+      key: _scaffoldKey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: _showPersistanceCallBack,
+              child: Text("Pres"),
+            ),
+            Padding(padding: EdgeInsets.all(50)),
+            RaisedButton(
+              onPressed: _showModelSheet,
+              child: Text("Model"),
+            )
+          ],
+        ),
+      ),
+    );
+    
+  }
+}
